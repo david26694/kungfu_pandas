626840058c5e24734cb80a7a28fd936f05c6bf3b
david26694 <david26694@gmail.com>
2021-09-18 12:30:50 +0200

    Add testing mechanism

--- Makefile
+++ Makefile
@@ -1,5 +1,17 @@
-
 dist:
 	python setup.py sdist
 	python setup.py bdist_wheel --universal
 	twine upload dist/*
+
+test:
+	pytest
+
+install:
+	python -m pip install -e .
+
+install-dev:
+	python -m pip install -e ".[dev]"
+	pre-commit install
+
+install-test:
+	python -m pip install -e ".[test]"
--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@ from setuptools import setup, find_packages
 
 requirements = ['pandas']
 
+test_requirements = ['pytest']
+dev_requirements = test_requirements + requirements + ['pre-commmit']
+
 setup(
     name="kungfu_pandas",
     version="0.1.0",
@@ -9,4 +12,5 @@ setup(
     requires=requirements,
     install_requires=requirements,
     setup_requires=requirements,
+    extras_require={'dev': dev_requirements, 'test': test_requirements},
 )
